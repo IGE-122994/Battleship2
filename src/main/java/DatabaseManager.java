ed8999fb2d8880f0e1b6b5d5932b6605e83fb18a
José Maria Remédios <jmbms3@iscte-iul.pt>
2026-03-11 16:52:30 +0000

    Adicionada funcionalidade de guardar histórico na base de dados SQLite.

---
+++ src/main/java/DatabaseManager.java
@@ -0,0 +1,7 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.SQLException;
+public class DatabaseManager {
+}
