5e1fdc3fd30429497ec9f1867d8e826b435f9a8c
Diogo Almeida <dasra2@iscte-iul.pt>
2026-03-20 14:57:21 +0000

    Revert "feature-internationalization-messages"

--- src/main/java/battleship/Main.java
+++ src/main/java/battleship/Main.java
@@ -3,7 +3,7 @@
  */
 
 package battleship;
-import java.util.Scanner;
+
 /**
  * The type Main.
  *
@@ -11,24 +11,19 @@ import java.util.Scanner;
  * @author adrianolopes
  * @author miguelgoulao
  */
-public class Main {
+public class Main
+{
+	/**
+	 * Main.
+	 *
+	 * @param args the args
+	 */
 
-    /**
-     * Main.
-     *
-     * @param args the args
-     */
+	public static void main(String[] args)
+    {
+		DatabaseManager.initializeDatabase();
+		System.out.println("***  Battleship  ***");
 
-    public static void main(String[] args) {
-        DatabaseManager.initializeDatabase();
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Escolha o idioma / Choose language (pt/en): ");
-        String language = scanner.nextLine().trim();
-
-        MessageManager.setLanguage(language);
-
-        System.out.println(MessageManager.get("game.title"));
-        Tasks.menu();
+		Tasks.menu();
     }
 }
