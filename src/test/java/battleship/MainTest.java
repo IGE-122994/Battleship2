44cff3bdf4d7d9777fbf4b5d8922cd95533c0278
vicencioo <pmgvo@iscte-iul.pt>
2026-04-23 00:49:16 +0100

    MainTest.java implemented

--- src/test/java/battleship/MainTest.java
+++ src/test/java/battleship/MainTest.java
@@ -7,14 +7,22 @@ import java.io.ByteArrayInputStream;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+/**
+ * Test class for Main.
+ * Author: Pedro Vicêncio
+ * Date: 2026-04-23
+ * Time: 14:00
+ * Cyclomatic Complexity for each method:
+ * - main: 1
+ */
 class MainTest {
 
     @DisplayName("Execução do main não lança exceções com input válido")
     @Test
-    void testMainDoesNotThrowException() {
+    void main() {
         String simulatedInput = "pt\n";
         System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
 
-        assertDoesNotThrow(() -> Main.main(new String[]{}));
+        assertDoesNotThrow(() -> Main.main(new String[]{}), "Error: Main.main should not throw any exceptions with valid input.");
     }
 }
